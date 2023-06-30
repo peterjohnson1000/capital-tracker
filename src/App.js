d58fef8203dd7515fd73865000276b22f47702ed
peterjohnson1000 <peterj1298@gmail.com>
2023-06-30 14:26:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import LineChart from './LineChart';
+import {capital} from "./data/capital"
 
 function App() {
+  const [capitalData, setcapitalData] = useState(
+    {
+      labels: capital.map((data) => data.date),
+      datasets: [
+        {
+          label: "Capital",
+          data: capital.map((data) => data.capital)
+        }
+      ]
+    }
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <LineChart capitalData={capitalData} />
     </div>
   );
 }
---
+++ src/data/capital.js
@@ -0,0 +1,22 @@
+export const capital = [
+    {
+        id: 1,
+        date: "24 May 23",
+        capital: 45043
+    },
+    {
+        id: 2,
+        date: "31 May 23",
+        capital: 45188.68
+    },
+    {
+        id: 3,
+        date: "21 June 23",
+        capital: 45309.36
+    },
+    {
+        id: 4,
+        date: "27 June 23",
+        capital: 45584.6
+    },
+]
