200c5d8e24caf3161f1a7085edbe787b9130e31f
peterjohnson1000 <peterj1298@gmail.com>
2023-07-04 19:04:14 +0530

    defined NoSQL db and serverless function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import LineChart from './LineChart';
 import {capital} from "./data/capital"
 
 function App() {
-  const [capitalData, setcapitalData] = useState(
+  const [capitalData, setCapitalData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "Capital",
+        data: []
+      }
+    ]
+  });
+
+  const [ncapitalData, nsetcapitalData] = useState(
     {
       labels: capital.map((data) => data.date),
       datasets: [
@@ -16,9 +26,31 @@ function App() {
     }
   );
 
+  useEffect(() => {
+    // Fetch the formatted data from the API or Lambda function
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://srq54buuybkdxmt45guraiwhrm0dexyn.lambda-url.ap-south-1.on.aws/');
+        const jsonData = await response.json();
+
+        setCapitalData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(capitalData);
+
   return (
-    <div className="">
-      <LineChart capitalData={capitalData} />
+    <div className="flex justify-center items-center">
+      <div className="w-[700px]">
+        <LineChart capitalData={capitalData} />
+        <LineChart capitalData={ncapitalData} />
+        <p>The below chart is just for verification</p>
+      </div>
     </div>
   );
 }
